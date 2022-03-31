40f630ef993c060ed48f6d25c02a123674cc6b81
gatin <gatin@LAPTOP-GECSPVNA>
2022-03-31 10:17:39 +0300

    Добавьте файлы проекта.

---
+++ BarsGroup_Events/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BarsGroup_Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo foo = new Foo();
+
+            foo.OnKeyPressed += Foo_OnKeyPressed;
+            Console.WriteLine("Для завершения программы нажмите C");
+            Console.WriteLine("Введите символ");
+            foo.Run();
+
+
+        }
+
+        private static void Foo_OnKeyPressed(Object sender, char ski)
+        {      
+            Console.WriteLine( $"{ski}");
+        }
+    }
+
+
+    class Foo
+    {
+        public event EventHandler<char>? OnKeyPressed;
+
+        ConsoleKeyInfo a;
+
+        public void Run()
+        {
+            while(Console.ReadKey().Key != ConsoleKey.C)
+            {
+               
+                a = Console.ReadKey();
+                char ski = a.KeyChar;
+
+                OnKeyPressed?.Invoke(this, ski);
+            }
+            
+        }
+    }
+}
